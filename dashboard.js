2d115b9dd42093869e004c1d99cef5830d30ff2b
Sheikh Muntaqim Ahmed Al Hasib <sheikhmuntaqimahmedalhasaibcode@gmail.com>
2026-03-22 23:54:15 +0600

    Add active tab switching functionality

--- dashboard.js
+++ dashboard.js
@@ -5,3 +5,13 @@ const tabs = document.querySelectorAll(".tabs button");
 let allIssues = [];
 
 let currentTab = "all";
+
+
+function setActiveTab(btn){
+
+    tabs.forEach(tab => {
+        tab.classList.remove("active");
+    });
+
+    btn.classList.add("active");
+}
